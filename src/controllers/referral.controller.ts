d9bb9726b5311bdcb22f3defdb68281a4987a7c1
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-07-06 10:02:27 +0530

    successfully create referral

---
+++ src/controllers/referral.controller.ts
@@ -0,0 +1,40 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { errorHandler } from "../utils/error";
+import { PrismaClient } from '@prisma/client';
+
+export const createReferral = async (req: Request, res: Response, next: NextFunction) => {
+  console.log('====================================');
+  console.log("req.body", req.body);
+  console.log('====================================');
+  const prisma = new PrismaClient();
+  const { name, email, referredBy } = req.body;
+
+  if (!name || !email || !referredBy) {
+    return next(errorHandler(400, "Missing fields"));
+    // return res.status(400).json({ error: 'Missing fields' });
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return next(errorHandler(400, "Invalid email"));
+    // return res.status(400).json({ error: 'Invalid email' });
+  }
+
+  try {
+    const referral = await prisma.referral.create({
+      data: { name, email, referredBy }
+    });
+
+    // await sendReferralEmail(name, email, referredBy);
+
+    res.status(201).json(referral);
+  } catch (error) {
+    if (error) {
+      return next(errorHandler(409, "Email already exists"));
+      // res.status(409).json({ error: 'Email already exists' });
+    } 
+    // else {
+    //   res.status(500).json({ error: 'Internal Server Error' });
+    // }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,31 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import bodyParser from 'body-parser';
+import referralRoutes from './routes/referral.route';
 
 const app = express();
-const prisma = new PrismaClient();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/api/referrals', async (req: Request, res: Response) => {
-  const { name, email, referredBy } = req.body;
-
-  if (!name || !email || !referredBy) {
-    return res.status(400).json({ error: 'Missing fields' });
-  }
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    return res.status(400).json({ error: 'Invalid email' });
-  }
-
-  try {
-    const referral = await prisma.referral.create({
-      data: { name, email, referredBy }
-    });
-
-    // await sendReferralEmail(name, email, referredBy);
-
-    res.status(201).json(referral);
-  } catch (error) {
-    if (error) {
-      res.status(409).json({ error: 'Email already exists' });
-    } 
-    // else {
-    //   res.status(500).json({ error: 'Internal Server Error' });
-    // }
-  }
-});
+app.use('/api/referral', referralRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+interface CustomError extends Error {
+  statusCode?: number;
+}
+
+//Error-Handling Middleware
+app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
