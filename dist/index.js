902e57f6bc5ddae425c4f465655660c5ce907c67
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-07-07 07:55:58 +0530

    some changes

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const referral_route_1 = __importDefault(require("./routes/referral.route"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
+app.use(body_parser_1.default.json());
+app.use('/api/referral', referral_route_1.default);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+//Error-Handling Middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+//# sourceMappingURL=index.js.map
